72c36d0393a6353ede3d7f44e931c7770aa0364f
wuxiaojiang <717189544@qq.com>
2015-01-27 13:45:36 +0800

    Update window.py

--- window.py
+++ window.py
@@ -1,26 +1,55 @@
+
+
 from ctypes import *
 
 UINT      = c_uint
 WNDPROC   = c_void_p
 INT       = c_int
-HINSTANCE = c_void_p
-HINCON    = c_void_p
+HANDLE    = c_int
 LPCTSTR   = POINTER(c_char)
+BOOL      = c_bool
+LONG      = c_long
 
 
+WNDPROC = WINFUNCTYPE(c_long,c_int,c_uint,c_int,c_int )
 
 class WNDCLASSEX(Structure):
     _fileds_=[
         ('cbSize',        UINT),
         ('style',         UINT),
+        ('lpfnWndProc',   WNDPROC),
         ('cbClsExtra',    INT),
         ('cbWndExtra',    INT),
-        ('hInstance',     HINSTANCE),
-        ('hIcon',         HINSTANCE),
-        ('hCursor',       HINSTANCE),
-        ('hbrBackground', HINSTANCE),
+        ('hInstance',     HANDLE),
+        ('hIcon',         HANDLE),
+        ('hCursor',       HANDLE),
+        ('hbrBackground', HANDLE),
         ('lpszMenuName',  LPCTSTR),
         ('lpszClassName', LPCTSTR),
-        ('hIconSm',       HINSTANCE),
+        ('hIconSm',       HANDLE),
         ]
-        
+
+class RECT(Structure):
+    _fileds_=[
+        ("left",          LONG),
+        ('top',           LONG),
+        ('right',         LONG),
+        ('bottom',        LONG),
+        ]
+    
+class PAINTSTRUCT(Struct):
+    _fileds_=[
+        ('hDc',           HANDLE),
+        ('fErase',        BOOL),
+        ('rcPaint',       RECT),
+        ('fRestore',      BOOL),
+        ('fIncUpdate',    BOOL),
+        ('rgbReserved[32]',c_char*32)
+        ]
+class 
+    
+
+
+
+
+
